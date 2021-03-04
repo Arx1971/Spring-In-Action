28958ea907eb45d43747c661f3f1547003d15314
Adnan Rahin <adnan.rahin13@gmail.com>
2021-03-04 18:53:42 -0500

    Spring-In-Action

---
+++ chapter-one/src/test/java/com/spring/in/action/chapterone/ChapterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.in.action.chapterone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
