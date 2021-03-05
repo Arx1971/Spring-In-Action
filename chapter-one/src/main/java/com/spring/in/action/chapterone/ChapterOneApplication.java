027325e93b55f9ff5a47c9c82068f400e34e09ef
Adnan Rahin <adnan.rahin13@gmail.com>
2021-03-04 19:34:51 -0500

    Chapter One is done

--- chapter-one/src/main/java/com/spring/in/action/chapterone/ChapterOneApplication.java
+++ chapter-one/src/main/java/com/spring/in/action/chapterone/ChapterOneApplication.java
@@ -2,8 +2,9 @@ package com.spring.in.action.chapterone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ChapterOneApplication {
 
     public static void main(String[] args) {
