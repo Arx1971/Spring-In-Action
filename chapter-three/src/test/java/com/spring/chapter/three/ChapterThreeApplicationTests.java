a6de8d0d5cb82f7f8a4fcbaa9a53f492278f6b70
Adnan Rahin <adnan.rahin13@gmail.com>
2021-03-05 20:35:56 -0500

    Spring In Action Chapter Three

---
+++ chapter-three/src/test/java/com/spring/chapter/three/ChapterThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.chapter.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
